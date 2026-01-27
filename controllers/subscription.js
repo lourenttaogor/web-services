198946ff7ff1ccbe5a3966d6e9f63f22f7bff3e6
Laurelpere <pelau360@gmail.com>
2026-01-27 23:42:24 +0100

    correction

--- controllers/subscription.js
+++ controllers/subscription.js
@@ -1,49 +1,30 @@
 const mongodb = require('../contacts/database');
 const ObjectID = require('mongodb').ObjectId;
 
-const getAllSubscription = async (req, res) => {
-  const result = await mongodb.getDatabase().db().collection('subscription').find();
-  result.toArray().then((contacts) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(contacts);
-  })
-}
-
-const getSingleSubscription = async (req, res) => {
-  const subscriptionid = new ObjectID(req.params.id)
-  const result = await mongodb.getDatabase().db().collection('subscription').find({ _id: subscriptionid });
-  result.toArray().then((subscriptions) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(subscriptions[0]);
-  })
-}
 
 const createSubscription = async (req, res) => {
   const subscriptions = {
-    _id: req.body._id,
-    userId: req.body.userId,
-    providerName: req.body.providerName,
+    userId: new ObjectID(req.body.userId), 
     monthlyCost: req.body.monthlyCost,
     category: req.body.category,
     renewalDate: req.body.renewalDate,
     paymentMethod: req.body.paymentMethod,
     status: req.body.status,
     description: req.body.description
-  }
+  };
 
   const response = await mongodb.getDatabase().db().collection('subscription').insertOne(subscriptions);
-  if (response.acknowledged){
-    res.status(204).send();
-  }else{
-    res.status(500).json(response.error)
+  if (response.acknowledged) {
+    res.status(204).json(response); 
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while creating the subscription.');
   }
-}
+};
 
 const updateSubscription = async (req, res) => {
-  const subscriptionid = new ObjectID(req.params.id)
-  
+  const subscriptionid = new ObjectID(req.params.id);
   const subscriptions = {
-    userId: req.body.userId,
+    userId: new ObjectID(req.body.userId),
     providerName: req.body.providerName,
     monthlyCost: req.body.monthlyCost,
     category: req.body.category,
@@ -51,26 +32,30 @@ const updateSubscription = async (req, res) => {
     paymentMethod: req.body.paymentMethod,
     status: req.body.status,
     description: req.body.description
-  }
+  };
 
-  const response = await mongodb.getDatabase().db().collection('subscription').find({ _id: subscriptionid }, subscriptions);
-  if (response.modifiedCount > 0){
+
+  const response = await mongodb.getDatabase().db().collection('subscription').replaceOne({ _id: subscriptionid }, subscriptions);
+  
+  if (response.modifiedCount > 0) {
     res.status(204).send();
-  }else{
-    res.status(500).json(response.error)
+  } else {
+    res.status(500).json(response.error || 'Update failed or no changes made.');
   }
-}
+};
 
 const deleteSubscription = async (req, res) => {
-  const subscriptionid = new ObjectID(req.params.id)
+  const subscriptionid = new ObjectID(req.params.id);
   
+  // FIX: deletedCount, not deleteCount
   const response = await mongodb.getDatabase().db().collection('subscription').deleteOne({ _id: subscriptionid });
-  if (response.deleteCount > 0){
+  
+  if (response.deletedCount > 0) {
     res.status(204).send();
-  }else{
-    res.status(500).json(response.error)
+  } else {
+    res.status(500).json(response.error || 'Delete failed - document might not exist.');
   }
-}
+};
 
 
 module.exports = {
--- controllers/users.js
+++ controllers/users.js
@@ -2,21 +2,21 @@ const mongodb = require('../contacts/database');
 const ObjectID = require('mongodb').ObjectId;
 
 const getAllUsers = async (req, res) => {
-  const result = await mongodb.getDatabase().db().collection('users').find();
-  result.toArray().then((user) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(user);
-  })
-}
+  const users = await mongodb.getDatabase().db().collection('users').find().toArray();
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).json(users);
+};
 
 const getSingleUsers = async (req, res) => {
-  const userid = new ObjectID(req.params.id)
-  const result = await mongodb.getDatabase().db().collection('users').find({ _id: userid });
-  result.toArray().then((user) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(user[0]);
-  })
-}
+  if (!ObjectID.isValid(req.params.id)) {
+    return res.status(400).json('Invalid ID format');
+  }
+  const userid = new ObjectID(req.params.id);
+  const user = await mongodb.getDatabase().db().collection('users').findOne({ _id: userid });
+  
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).json(user);
+};
 
 const createUsers = async (req, res) => {
   const user = {
@@ -24,46 +24,49 @@ const createUsers = async (req, res) => {
     email: req.body.email,
     firstName: req.body.firstName,
     lastName: req.body.lastName,
-    accountCreated: req.body.accountCreated
-  }
+    accountCreated: req.body.accountCreated || new Date() 
+  };
 
   const response = await mongodb.getDatabase().db().collection('users').insertOne(user);
-  if (response.acknowledged){
-    res.status(204).send();
-  }else{
-    res.status(500).json(response.error)
+  if (response.acknowledged) {
+    res.status(201).json(response); 
+  } else {
+    res.status(500).json('An error occurred while creating the user.');
   }
-}
+};
 
 const updateUsers = async (req, res) => {
-  const userid = new ObjectID(req.params.id)
-  
+  const userid = new ObjectID(req.params.id);
   const user = {
     googleId: req.body.googleId,
     email: req.body.email,
     firstName: req.body.firstName,
+    lastName: req.body.lastName,
     accountCreated: req.body.accountCreated
-  }
+  };
 
-  const response = await mongodb.getDatabase().db().collection('users').find({ _id: userid }, user);
-  if (response.modifiedCount > 0){
+  
+  const response = await mongodb.getDatabase().db().collection('users').replaceOne({ _id: userid }, user);
+  
+  if (response.modifiedCount > 0) {
     res.status(204).send();
-  }else{
-    res.status(500).json(response.error)
+  } else {
+    res.status(500).json('Update failed or no changes made.');
   }
-}
+};
 
 const deleteUsers = async (req, res) => {
-  const userid = new ObjectID(req.params.id)
+  const userid = new ObjectID(req.params.id);
+  
   
   const response = await mongodb.getDatabase().db().collection('users').deleteOne({ _id: userid });
-  if (response.deleteCount > 0){
+  
+  if (response.deletedCount > 0) {
     res.status(204).send();
-  }else{
-    res.status(500).json(response.error)
+  } else {
+    res.status(500).json('Could not find the user to delete.');
   }
-}
-
+};
 
 module.exports = {
   getSingleUsers,
