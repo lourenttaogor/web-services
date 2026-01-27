d3ef40a4d6037f400d1f03f670be76f7317c7889
Laurelpere <pelau360@gmail.com>
2026-01-27 23:48:28 +0100

    correction

--- controllers/subscription.js
+++ controllers/subscription.js
@@ -1,10 +1,28 @@
 const mongodb = require('../contacts/database');
 const ObjectID = require('mongodb').ObjectId;
 
+const getAllSubscription = async (req, res) => {
+  const result = await mongodb.getDatabase().db().collection('subscription').find();
+  result.toArray().then((contacts) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(contacts);
+  })
+}
+
+const getSingleSubscription = async (req, res) => {
+  const subscriptionid = new ObjectID(req.params.id)
+  const result = await mongodb.getDatabase().db().collection('subscription').find({ _id: subscriptionid });
+  result.toArray().then((subscriptions) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(subscriptions[0]);
+  })
+}
+
 
 const createSubscription = async (req, res) => {
   const subscriptions = {
-    userId: new ObjectID(req.body.userId), 
+    userId: new ObjectID(req.body.userId),
+    providerName: req.body.providerName,
     monthlyCost: req.body.monthlyCost,
     category: req.body.category,
     renewalDate: req.body.renewalDate,
@@ -15,7 +33,7 @@ const createSubscription = async (req, res) => {
 
   const response = await mongodb.getDatabase().db().collection('subscription').insertOne(subscriptions);
   if (response.acknowledged) {
-    res.status(204).json(response); 
+    res.status(201).json(response); 
   } else {
     res.status(500).json(response.error || 'Some error occurred while creating the subscription.');
   }
@@ -34,7 +52,7 @@ const updateSubscription = async (req, res) => {
     description: req.body.description
   };
 
-
+ 
   const response = await mongodb.getDatabase().db().collection('subscription').replaceOne({ _id: subscriptionid }, subscriptions);
   
   if (response.modifiedCount > 0) {
@@ -47,7 +65,6 @@ const updateSubscription = async (req, res) => {
 const deleteSubscription = async (req, res) => {
   const subscriptionid = new ObjectID(req.params.id);
   
-  // FIX: deletedCount, not deleteCount
   const response = await mongodb.getDatabase().db().collection('subscription').deleteOne({ _id: subscriptionid });
   
   if (response.deletedCount > 0) {
